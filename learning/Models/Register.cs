7d9e3457ce3f355afb15a1bfcb90582a269be11c
EliasH444 <EliasH444@outlook.com>
2025-03-08 12:23:54 +0000

    Created sign up models

---
+++ learning/Models/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace learning.Models
+{
+    public class Register
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ learning/Models/SignUp.cs
@@ -0,0 +1,10 @@
+﻿namespace learning.Models
+{
+    public class SignUp
+    {
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
